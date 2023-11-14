4848625c5a39d8be67545432cec27c3aae23252c
Giacomo Galloni <giacomo.galloni@roma2.infn.it>
2023-11-14 18:38:04 +0100

    Add assert values of likelihood

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -13,12 +13,13 @@ mean = np.array([0.30245268, 0.61884443, 0.5])
 cov = np.array(
     [[0.00796336, -0.0014805, -0.00479433], [-0.0014805, 0.00561415, 0.00434189],
      [-0.00479433, 0.00434189, 0.03208593]])
+sigma_c = np.sqrt(cov[2, 2])
 _inv_cov = np.linalg.inv(cov)
 
 
 class NoisyCovLike(Likelihood):
     params = {'a': [0, 1, 0.5, 0.3, 0.08], 'b': [0, 1, 0.5, 0.3, 0.08]}
-    params['c'] = {'value': [0.4, 0.5, 0.6], 'profiled': True}
+    params['c'] = {'value': [mean-sigma_c, mean, mean+sigma_c], 'profiled': True}
     noise = 0
 
     def logp(self, **params_values):
@@ -28,12 +29,18 @@ class NoisyCovLike(Likelihood):
 
 @mpi.sync_errors
 def test_profile_gaussian(tmpdir):
-    maxloglik = 0
+    maxlogliks = [1, 0, 1]
     for method in reversed(valid_methods):
         NoisyCovLike.noise = 0.005 if method == 'bobyqa' else 0
         info: InputDict = {'likelihood': {'like': NoisyCovLike},
                            "sampler": {"minimize": {"ignore_prior": True,
                                                     "method": method}}}
         samplers = run(info)[1]
-        print(sampler.products() for sampler in samplers)
+        errors = []
+        for i, sampler in enumerate(samplers):
+            products = sampler.products()
+            errors.append(abs(maxlogliks[i] - -products["minimum"]["minuslogpost"]))
+        assert errors[0] < 0.01
+        assert errors[1] < 0.01
+        assert errors[2] < 0.01
         return
