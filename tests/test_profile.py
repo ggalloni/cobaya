a907988c3c5dc529755de1a178db03dbb65a1633
Giacomo Galloni <giacomo.galloni@roma2.infn.it>
2023-11-15 17:53:54 +0100

    Cleaned tests and bug fix

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -14,13 +14,21 @@ mean = np.array([0.30245268, 0.61884443, 0.5])
 cov = np.array(
     [[0.00796336, -0.0014805, -0.00479433], [-0.0014805, 0.00561415, 0.00434189],
      [-0.00479433, 0.00434189, 0.03208593]])
-sigma_c = np.sqrt(cov[2, 2])
 _inv_cov = np.linalg.inv(cov)
 
+mean_c = mean[2]
+sigma_c = round(np.sqrt(cov[2, 2]), 3)
+profiled_values = [
+    mean_c - 2 * sigma_c,
+     mean_c - sigma_c,
+     mean_c,
+     mean_c + sigma_c,
+     mean_c + 2 * sigma_c
+]
 
 class NoisyCovLike(Likelihood):
     params = {'a': [0, 1, 0.5, 0.3, 0.08], 'b': [0, 1, 0.5, 0.3, 0.08]}
-    params['c'] = {'value': [mean[2]-sigma_c, mean[2], mean[2]+sigma_c], 'profiled': True}
+    params['c'] = {'value': profiled_values, 'profiled': True}
     noise = 0
 
     def logp(self, **params_values):
@@ -30,20 +38,16 @@ class NoisyCovLike(Likelihood):
 
 @mpi.sync_errors
 def test_profile_gaussian(tmpdir):
-    maxlogliks = [0.5, 0, 0.5]
+    loglike_vals = [2, 0.5, 0, 0.5, 2]
     for method in reversed(valid_methods):
         NoisyCovLike.noise = 0.005 if method == 'bobyqa' else 0
         info: InputDict = {'likelihood': {'like': NoisyCovLike},
                            "sampler": {"minimize": {"ignore_prior": True,
                                                     "method": method}}}
         samplers = run(info)[1]
-        errors = []
         for i, sampler in enumerate(samplers):
             products = sampler.products()
-            errors.append(abs(maxlogliks[i] - products["minimum"]["minuslogpost"]))
-        assert errors[0] < 0.01
-        assert errors[1] < 0.01
-        assert errors[2] < 0.01
+            assert abs(loglike_vals[i] - products["minimum"]["minuslogpost"]) < 0.01
         return
 
 
@@ -52,6 +56,7 @@ def test_profile_output(tmpdir):
     for method in reversed(valid_methods):
         NoisyCovLike.noise = 0.005 if method == 'bobyqa' else 0
         info: InputDict = {'likelihood': {'like': NoisyCovLike},
+                           'force': True,
                            "sampler": {"minimize": {"ignore_prior": True,
                                                     "method": method}}}
         info['output'] = os.path.join(tmpdir, 'testprof')
@@ -61,6 +66,6 @@ def test_profile_output(tmpdir):
             with open(filename, "rb") as f:
                 res = dill.load(f)
             assert isinstance(res, dict)
-            keys = list(res.keys())
-            expected_keys = ['value', 'minimum', 'full_set_of_mins', 'hessian', 'other_params']
+            keys = list(res["c"].keys())
+            expected_keys = ['values', 'minima', 'full_sets_of_mins', 'hessians', 'other_params']
             assert all(k in expected_keys for k in keys)
